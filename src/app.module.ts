4d71872aeff5668f4a1aef449640f81ca2c12483
LongNgo <ngovucaolong9118.thd@gmail.com>
2024-08-16 15:41:06 +0700

    reset branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,41 +1,39 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import {TypeOrmModule} from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth_module/auth.module';
 import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from './common/guards/jwt.guard'; 
+import { JwtAuthGuard } from './common/guards/jwt.guard';
 import { CacheModule } from '@nestjs/cache-manager';
 import * as redisStore from 'cache-manager-redis-store';
 import { typeOrmConfig, typeOrmConfigASync } from './config/typeorm.config';
 import { GeminiModule } from './gemini_module/gemini.module';
-import * as dotenv from 'dotenv'
-import { TracingMiddlware } from './tracing/tracing.middleware';
-import { TracingModule } from './tracing/tracing.module';
-dotenv.config()
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal:true,
-      load : [()=>({...process.env})]
-    }),TypeOrmModule.forRootAsync(typeOrmConfigASync),
+      isGlobal: true,
+      load: [() => ({ ...process.env })],
+    }),
+    TypeOrmModule.forRootAsync(typeOrmConfigASync),
     CacheModule.register({
-    isGlobal: true,
-    ttl:0,
-    max:10,  
-    store: redisStore,
-    host: 'localhost',
-    port: 6379,
-
-  }), AuthModule , GeminiModule, TracingModule],
-  providers:[{
-    provide: APP_GUARD,
-    useClass: JwtAuthGuard,
-  }]
-
+      isGlobal: true,
+      ttl: 0,
+      max: 10,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+    AuthModule,
+    GeminiModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
-export class AppModule implements NestModule{
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(TracingMiddlware)
-    .forRoutes('*');
-  }
-}
+export class AppModule {}
